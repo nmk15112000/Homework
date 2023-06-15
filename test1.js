6ad235fb99ebf35f1bf3bac80db040221f418806
nmk15112000 <mauquipro1@gmail.com>
2023-06-15 22:55:37 +0700

    Push 1

---
+++ test1.js
@@ -0,0 +1,14 @@
+// Start phần bài 1
+// const array1 = [1,2,3,4,5,6];
+// const array2 = [3,4,8,9,12];
+// console.log(array2) // 
+// console.log(array1) // 
+// const intersection = array1.filter(element => {
+// 	return array2.includes(element);
+//   });
+  
+//   console.log(intersection); 
+//  End phần bài 1
+// Start phần bài 2
+let a =[1,7,9,2,5,3,8]
+let highest = math.max(a)
