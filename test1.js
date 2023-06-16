a0f2ec5553371dc7c3320aa64aeccbf3b3a01de3
nmk15112000 <mauquipro1@gmail.com>
2023-06-16 10:43:10 +0700

    push 2

--- test1.js
+++ test1.js
@@ -10,5 +10,24 @@
 //   console.log(intersection); 
 //  End phần bài 1
 // Start phần bài 2
-let a =[1,7,9,2,5,3,8]
-let highest = math.max(a)
+// {function greatestSum(arr) {
+  
+//     let max1 = 0;
+//     let max2 = 0;
+  
+//     for (let i = 2; i < arr.length; i++) {
+//       if (arr[i] > max1) {
+//         max2 = max1;
+//         max1 = arr[i];
+//       } else if (arr[i] > max2) {
+//         max2 = arr[i];
+//       }
+//     }
+  
+//     return [max1, max2];
+//   }
+  
+//   const array = [1, 7, 9, 2, 5, 3, 8];
+//   const result = greatestSum(array);
+//   console.log(result);}
+//  End phần bài 2
